1943b24ecce1447114f97b82d77832ecb1409ae3
Harish Rathi <harish.rathi@algosystech.com>
2024-08-17 16:48:46 +0530

    separate css per components

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-team',
-  standalone: true,
-  imports: [],
-  templateUrl: './team.component.html',
-  styleUrl: './team.component.css'
+    selector: 'app-team',
+    standalone: true,
+    imports: [],
+    templateUrl: './team.component.html',
+    styleUrl: './team.component.css'
 })
 export class TeamComponent {
 
+    public onMoreClick(id: string) {
+        const element = document.getElementById(id);
+        if (element == null)
+            return;
+        element.style.height = element.style.height !== '6em'
+            ? '6em' : 'auto';
+    }
 }
